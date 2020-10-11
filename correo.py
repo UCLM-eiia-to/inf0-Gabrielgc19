32986e1c360977a527eb32fedab36f71a5f3e59d
Gabrielgc19 <72700158+Gabrielgc19@users.noreply.github.com>
2020-10-11 17:33:13 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,3 +1,6 @@
-# Define aquí mismo la función my_email sin argumentos que 
+# Define aquí mismo la función my_email
+def my_email():
 # devuelve tu correo electrónico de la UCLM
+    return 'gabriel.garcia13@alu.uclm.es'
+
 
